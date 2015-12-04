5e8dd95da3b4c8f7735deb555989332ffa2ba9b5
i309511 <harrifeng@gmail.com>
2015-12-04 00:34:11 -0800

    extra middleware

--- main.go
+++ main.go
@@ -11,17 +11,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %v\n", bone.GetValue(r, "id"))
 }
 
-func MyMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	fmt.Println("Hello----------->")
+func CommonMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	fmt.Println("All----------->")
 	next(rw, r)
-	fmt.Println("World===========>")
+	fmt.Println("All----------->")
+}
+
+func AdminMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	fmt.Println("Admin----------->")
+	next(rw, r)
+	fmt.Println("Admin----------->")
 }
 
 func main() {
 	mux := bone.New()
 	mux.Get("/some/page/:id", http.HandlerFunc(handler))
+	// Special treat, extra middleware for admin
+	mux.Get("/admin", negroni.New(negroni.HandlerFunc(AdminMiddleware)))
+
 	n := negroni.Classic()
-	n.Use(negroni.HandlerFunc(MyMiddleware))
+	n.Use(negroni.HandlerFunc(CommonMiddleware))
 	n.UseHandler(mux)
 	n.Run(":3003")
 }
