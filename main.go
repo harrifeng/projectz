0e8ab61cbcd1eacb6908b6a8e5d6c02dbd441e11
i309511 <harrifeng@gmail.com>
2015-12-03 23:25:30 -0800

    use a customer middleware

--- main.go
+++ main.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"github.com/codegangsta/negroni"
 	"github.com/go-zoo/bone"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %v\n", bone.GetValue(r, "id"))
 }
 
+func MyMiddleware(rw http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+	fmt.Println("Hello----------->")
+	next(rw, r)
+	fmt.Println("World===========>")
+}
+
 func main() {
 	mux := bone.New()
 	mux.Get("/some/page/:id", http.HandlerFunc(handler))
 	n := negroni.Classic()
+	n.Use(negroni.HandlerFunc(MyMiddleware))
 	n.UseHandler(mux)
 	n.Run(":3003")
 }
-
